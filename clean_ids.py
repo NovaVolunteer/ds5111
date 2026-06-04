307b599733d973fb9833e83553a94ccf825cbbb6
NovaVolunteer <bwright.wright6@gmail.com>
2026-06-04 18:02:32 +0000

    add content for lab 2

---
+++ clean_ids.py
@@ -0,0 +1,23 @@
+import sys
+import re
+import logging
+
+logging.basicConfig(
+	filename="pipeline_audit.log",
+	level=logging.WARNING, #sets the minimal level of severity to get logged
+	format="%(asctime)s - %(message)s") #formats the message with a time stamp and the message
+
+pattern_ids = re.compile(r'^[A-Za-z0-9_-]{11}$') # compiling the pattern outside of the loop
+
+
+for line in sys.stdin:
+	line = line.strip()
+	if not line: # if theres a empty space for some reason
+		continue
+	if pattern_ids.match(line):
+		sys.stdout.write(line + "\n")
+		sys.stdout.flush() #sends out the ids as they are processed, doesn't hold anything in memory
+	else:
+		logging.error(f"Invalid YouTube ID: '{line}'")
+
+
